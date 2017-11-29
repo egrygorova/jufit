c295050c59a447fdf900895f0dc637444951793b
Elena Grygorova <elena.grygorova@uni-jena.de>
2017-11-29 22:04:15 +0100

    Update TransformToConceptMapperDict.java

--- src/main/java/de/julielab/extension/TransformToConceptMapperDict.java
+++ src/main/java/de/julielab/extension/TransformToConceptMapperDict.java
@@ -1,5 +1,3 @@
-package de.julielab.extension;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,8 +8,6 @@ import java.util.List;
 
 import javax.xml.bind.JAXB;
 
-import com.aliasi.classify.ConditionalClassifierEvaluator;
-
 public class TransformToConceptMapperDict {
 	static List<String> dictTokens = new ArrayList<>();
 	static List<DictToken> dictTokensXML = new ArrayList<>();
@@ -19,7 +15,7 @@ public class TransformToConceptMapperDict {
 	public static void main(String args[]) {
 
 		try {
-			String content = new String(Files.readAllBytes(Paths.get("src/main/resources/gazetterTest")));
+			String content = new String(Files.readAllBytes(Paths.get("gazetterTest")));
 			String[] entrys = content.split("\n");
 			String s = null;
 			for (int i = 0; i < entrys.length; i++) {
@@ -28,7 +24,7 @@ public class TransformToConceptMapperDict {
 			}
 
 			for (String dictToken : dictTokens) {
-				// System.out.println(dictToken);
+			// System.out.println(dictToken);
 				createDictToken(dictToken);
 			}
 		} catch (IOException e) {
@@ -43,30 +39,29 @@ public class TransformToConceptMapperDict {
 		// jaxbMarshaller.marshal(token, file);
 		// jaxbMarshaller.marshal(token, System.out);
 
-		Variant v1 = new Variant();
-		v1.setBase("Schmerz abdominal");
+//		Variant v1 = new Variant();
+//		v1.setBase("Schmerz abdominal");
+//
+//		Variant v2 = new Variant();
+//		v2.setBase("Hirnnerv VI");
+//
+//		DictToken token = new DictToken();
+//		token.setCanonical("Bauchschmerz");
+//		token.setCodeType("UMLS");
+//		token.setCodeValue("C0000737");
+//		token.setVariants(Arrays.asList(v1));
+//
+//		DictToken token2 = new DictToken();
+//		token2.setCanonical("Bauchmuskeln");
+//		token2.setCodeType("UMLS");
+//		token2.setCodeValue("C0000739");
+//
+//		DictToken token3 = new DictToken();
+//		token3.setCanonical("Nervus abducens");
+//		token3.setCodeType("UMLS");
+//		token3.setCodeValue("C0000741");
+//		token3.setVariants(Arrays.asList(v2));
 
-		Variant v2 = new Variant();
-		v2.setBase("Hirnnerv VI");
-
-		DictToken token = new DictToken();
-		token.setCanonical("Bauchschmerz");
-		token.setCodeType("UMLS");
-		token.setCodeValue("C0000737");
-		token.setVariants(Arrays.asList(v1));
-
-		DictToken token2 = new DictToken();
-		token2.setCanonical("Bauchmuskeln");
-		token2.setCodeType("UMLS");
-		token2.setCodeValue("C0000739");
-
-		DictToken token3 = new DictToken();
-		token3.setCanonical("Nervus abducens");
-		token3.setCodeType("UMLS");
-		token3.setCodeValue("C0000741");
-		token3.setVariants(Arrays.asList(v2));
-
-		createXML(token);
 
 	}
 
@@ -75,37 +70,58 @@ public class TransformToConceptMapperDict {
 		List<String> codeTypes = new ArrayList<>();
 		List<String> codeValues = new ArrayList<>();
 
-		String[] parts = dictToken.split("\t");
-		for (String part : parts) {
-			// System.out.println(part);
-			// System.out.println("-------------");
-		}
+		String[] parts = dictToken.split("\t|\\s+");
+//		for (String part : parts) {
+////			 System.out.println(part);
+////			 System.out.println("-------------");
+//		}
 
 		for (int i = 0; i < parts.length; i++) {
-			if (i % 3 == 0) {
+			if (parts[i].contains("UMLS")) {
+				codeTypes.add(parts[i]);
+			}
+
+			else if (parts[i].matches("[C]{1}\\d{7}")) {
+				codeValues.add(parts[i]);
+			} else if (!(parts[i].matches("[C]{1}\\d{7}") && parts[i].contains("UMLS"))) {
 				canonicals.add(parts[i]);
 			}
 
-
 		}
 
-		for (String cv : canonicals) {
-			System.out.println(cv);
-		}
-
+		 for (String cs : canonicals) {
+		 System.out.println(cs);
+		 }
+		 
+		 for (String codeType : codeTypes) {
+		 System.out.println(codeType);
+		 }
+		 
+		 for (String cc : codeValues) {
+		 System.out.println(cc);
+		 }
+		 
+		 createTokensForXML(canonicals, codeTypes, codeValues);
+		 
 		// TODO jedes erste part muss in canonicals Liste
 		// jedes zweite part muss in codeTypes Liste
 		// jedes dritte part muss in codeValues Liste
 
 	}
 
-	private static void createTokensForXML(String canonical, String codeType, String codeValue,
-			List<Variant> variants) {
+	private static void createTokensForXML(List<String> canonicals, List<String> codeTypes, List<String> codeValues) {
 		DictToken token = new DictToken();
-		token.setCanonical(canonical);
-		token.setCodeType(codeType);
-		token.setCodeValue(codeValue);
-		token.setVariants(variants);
+		for (String canonical : canonicals) {
+			token.setCanonical(canonical);
+		}
+		for (String codeType : codeTypes) {
+			token.setCodeType(codeType);
+		}
+		for (String codeValue : codeValues) {
+			token.setCodeValue(codeValue);
+		}
+
+		//token.setVariants(variants);
 		createXML(token);
 	}
 
@@ -115,12 +131,12 @@ public class TransformToConceptMapperDict {
 		Dictionary dict = new Dictionary();
 		dict.setTokens(dictTokensXML);
 
-		File file = new File("src/main/resources/gazetter.xml");
+		File file = new File("gazetter.xml");
 		JAXB.marshal(dict, file);
 	}
 
 	private static String extractContent(String entry) {
-
+		
 		entry = entry.replaceAll("@+", "");
 
 		entry = entry.replaceAll("ANY", "");
